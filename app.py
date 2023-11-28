ce2d55afffbe7e58d8265916ee9ecea901bb6351
i12hurel <i12hurel@uco.es>
2023-11-28 20:41:45 +0100

    segundo commit

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 import os
 
 app = Flask(__name__)
 
+# Lista de preguntas de la encuesta
+preguntas_encuesta = [
+    "¿Cómo calificarías nuestro servicio?",
+    "¿Cuál es tu característica favorita de nuestro producto?",
+    "¿Qué mejorarías en nuestra empresa?",
+]
+
+# Almacenar respuestas de la encuesta
+respuestas_encuesta = {pregunta: [] for pregunta in preguntas_encuesta}
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html', preguntas=preguntas_encuesta)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    for pregunta in preguntas_encuesta:
+        respuesta = request.form.get(pregunta)
+        respuestas_encuesta[pregunta].append(respuesta)
+
+    return redirect('/gracias')
+
+@app.route('/gracias')
+def gracias():
+    return "¡Gracias por completar la encuesta!"
 
 if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 8080
-    port = int(port)
-
-    app.run(port=port,host='0.0.0.0')
+    # Obtener el puerto de OpenShift si está disponible, de lo contrario, usar 8080
+    port = int(os.environ.get('PORT', 8080))
+    
+    # Configuración para permitir conexiones desde cualquier origen
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    
+    # Ejecutar la aplicación
+    app.run(port=port, host='0.0.0.0', debug=True)
